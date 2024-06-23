d0ac6b492c655fbc3a3876f8194b3fd9cb73c2b4
Koren Levi <koren012349@gmail.com>
2024-06-23 14:46:33 +0300

    koren test

--- app/src/main/java/com/example/smartqueue/MainActivity.java
+++ app/src/main/java/com/example/smartqueue/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 //shirel test
+//    koren
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
