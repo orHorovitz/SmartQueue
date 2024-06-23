42a99b92e39df2fe9bef65e4a739d1c9bbfda729
Abstract Programmer <AbstractProgrammer@example.com>
2024-06-23 14:47:55 +0300

    shirtel

--- app/src/main/java/com/example/smartqueue/MainActivity.java
+++ app/src/main/java/com/example/smartqueue/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 //shirel test
 //    koren
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
